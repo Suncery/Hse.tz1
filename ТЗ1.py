5fb0fa6af6e0550e6ab5a30b142e108f8024bbbf
Suncery <113044320+Suncery@users.noreply.github.com>
2022-09-29 23:37:38 +0300

    Add files via upload

--- ТЗ1.py
+++ ТЗ1.py
@@ -1,11 +1,14 @@
 from io import FileIO
-from typing import List
+from typing import List, Dict
 
 message = '''
 Выберите опцию:
 1 - Просмотр контактов
 2 - Редактирование контактов
 3 - Поиск контакта по телефону
+4 - Поиск контакта по email
+5 - Поиск контакта по ФИО
+6 - Вывод контактов с пустыми полями
 '''
 message2 = '''
 Выберите что редактировать:
@@ -13,8 +16,6 @@ message2 = '''
 2 - Телефон
 3 - Email
 '''
-
-
 class Contact:
     def __init__(self, phone: str = "", bio: str = "", email: str = ""):
         self.phone = phone
@@ -25,16 +26,23 @@ class Contact:
         return f'{self.phone}{delimeter}{self.bio}{delimeter}{self.email}'
 
 
-def deserialize(contact: str, delimiter=','):
+def deserialize(contact: str, delimiter=',')->Contact:
+    '''Десериализует объект типа Contact из строчки'''
     a, b, c = contact.split(delimiter)
     return Contact(a, b, c)
 
 def show_contacts(contacts):
+    '''Выводит контакты в консоль'''
     print('ID\tНомер\tФИО\tEmail')
     for i in range(len(contacts)):
         print(str(i)+'\t'+contacts[i].serialized("\t"))
-
-def edit_contact_dialogue(contacts):
+def show_contacts_keep_ids(contacts:Dict[int,Contact]):
+    '''Выводит контакты в консоль, сохраняя оригинальные ID'''
+    print('ID\tНомер\tФИО\tEmail')
+    for i in contacts:
+        print(str(i)+'\t'+contacts[i].serialized("\t"))
+def edit_contact_dialogue(contacts)->List[Contact]:
+    '''Замены данных на list объектов Contact'''
     while True:
         try:
             id = int(input("Введите ID контакта:"))
@@ -56,6 +64,7 @@ def edit_contact_dialogue(contacts):
     return contacts
 
 def update(file:FileIO,contacts:List[Contact]):
+    '''Перезаписывает файл новыми данными из contacts'''
     file.seek(0)
     for i in contacts:
         file.write(i.serialized()+'\n')
@@ -63,9 +72,19 @@ def update(file:FileIO,contacts:List[Contact]):
     file.flush()
 
 def search_by_phone(phone_number:str,contacts:List[Contact]):
-
-    show_contacts([x for x in contacts if phone_number in x.phone])
+    '''Выводит в консоль список контактов с телефоном содержащим поисковое значение'''
+    show_contacts_keep_ids({id:x for id,x in enumerate(contacts) if phone_number in x.phone})
+def search_by_email(email:str,contacts:List[Contact]):
+    '''Выводит в консоль список контактов с email-ом содержащим поисковое значение'''
+    show_contacts_keep_ids({id:x for id,x in enumerate(contacts) if email in x.email})
+def search_by_name(name_query:str,contacts:List[Contact]):
+    '''Выводит в консоль список контактов с именем содержащим поисковое значение'''
+    show_contacts_keep_ids({id:x for id,x in enumerate(contacts) if name_query in x.bio})
+def search_empty_fields(contacts:List[Contact]):
+    '''Выводит в консоль список контактов с пустым значением в полях email и phone'''
+    show_contacts_keep_ids({id: x for id, x in enumerate(contacts) if (not x.email or not x.phone)})
 def start_main_loop(file: FileIO):
+    '''Запускает главный диалог взаимодействия с программой'''
     contacts = []
     for contact in file.read().split('\n'):
         if contact:
@@ -85,10 +104,15 @@ def start_main_loop(file: FileIO):
             update(file,contacts)
         elif choice == 3:
             search_by_phone(input("Введите телефон для поиска:"),contacts)
-
+        elif choice==4:
+            search_by_email(input("Введите email для поиска:"), contacts)
+        elif choice==5:
+            search_by_name(input("Введите имя для поиска:"), contacts)
+        elif choice==6:
+            search_empty_fields(contacts)
 if __name__ == "__main__":
     file_name = input("Введите адрес файла контактов:")
-    with open(file_name, 'r+') as file:
+    with open(file_name, 'r+',encoding='utf-8') as file:
         start_main_loop(file)
 
 
